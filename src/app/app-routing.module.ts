3ada54537d40c37f8c99f7f83200a907edc5a693
geniinho <geniinho@hotmail.com>
2021-09-16 11:31:02 -0300

    html e css para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
@@ -9,9 +10,12 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
 
 const routes: Routes = [
   {
+    path:'login', component: LoginComponent
+  },
+  {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
-      { path: 'tecnicos',            component:   TecnicoListComponent },
+      { path: 'tecnicos', component:   TecnicoListComponent },
 
     ]
   }
