58068b39a49c8b6536181a87253cbb240c7e0fa9
geniinho <geniinho@hotmail.com>
2021-09-17 09:03:08 -0300

    interceptor para requisições http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptor/auth.interceptor';
 
 
 @NgModule({
@@ -75,7 +76,7 @@ import { ToastrModule } from 'ngx-toastr';
     })
 
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
