919598aabcf1e766c2bb5781d182221be5b84d83
geniinho <geniinho@hotmail.com>
2021-09-18 09:49:24 -0300

    delete finalizado

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -13,25 +13,25 @@ import { TecnicoService } from 'src/app/services/tecnico.service';
 export class TecnicoCreateComponent implements OnInit {
 
   tecnico: Tecnico = {
-    id: '',
-    nome: '',
-    cpf: '',
-    email: '',
-    senha: '',
-    perfis: [],
+    id:         '',
+    nome:       '',
+    cpf:        '',
+    email:      '',
+    senha:      '',
+    perfis:     [],
     dataCriacao: ''
   }
 
-  nome: FormControl = new FormControl(null, Validators.minLength(3));
-  cpf: FormControl = new FormControl(null, Validators.required);
-  email: FormControl = new FormControl(null, Validators.email);
+  nome: FormControl =  new FormControl(null, Validators.minLength(3));
+  cpf: FormControl =       new FormControl(null, Validators.required);
+  email: FormControl =        new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
   constructor(
     private service: TecnicoService,
-    private toast: ToastrService,
-    private router: Router,
-  ) { }
+    private toast:    ToastrService,
+    private router:          Router,
+    ) { }
 
   ngOnInit(): void { }
 
@@ -40,7 +40,7 @@ export class TecnicoCreateComponent implements OnInit {
       this.toast.success('Técnico cadastrado com sucesso', 'Cadastro');
       this.router.navigate(['tecnicos'])
     }, ex => {
-      if (ex.error.errors) {
+      if(ex.error.errors) {
         ex.error.errors.forEach(element => {
           this.toast.error(element.message);
         });
@@ -51,16 +51,16 @@ export class TecnicoCreateComponent implements OnInit {
   }
 
   addPerfil(perfil: any): void {
-    if (this.tecnico.perfis.includes(perfil)) {
+    if(this.tecnico.perfis.includes(perfil)) {
       this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
     } else {
       this.tecnico.perfis.push(perfil);
     }
-
+    
   }
-
+  
   validaCampos(): boolean {
     return this.nome.valid && this.cpf.valid
-      && this.email.valid && this.senha.valid
+     && this.email.valid && this.senha.valid
   }
 }
